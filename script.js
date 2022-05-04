f776b508ea5be677527db86eba9c97db408cdcde
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-05-04 09:06:47 -0600

    set up skeleton files

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+const mainApp = function () {
+    const log = (i) => console.log('\n', i);
+    log(`Hi Athma!`);
+};
+document.addEventListener('DOMContentLoaded', mainApp);
---
+++ script.ts
@@ -0,0 +1,6 @@
+const mainApp = function () {
+  const log = (i: unknown) => console.log('\n', i);
+
+  log(`Hi Athma!`);
+};
+document.addEventListener('DOMContentLoaded', mainApp);
