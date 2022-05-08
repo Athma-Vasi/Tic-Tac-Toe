3d273975994c3c2eef520080db37220afb9ff356
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-05-07 19:02:31 -0600

    add player symbols, add fieldset to disable double click

--- script.js
+++ script.js
@@ -31,43 +31,101 @@ const mainApp = function () {
     const gameBoardModule = (function () {
         //grabs the elements and sets up event listeners
         const form = document.querySelector('.form-startBttn');
+        let onceFlag = true;
+        let player1Turn = true;
+        let player2Turn = false;
+        let cells = [];
         function handleStartBttn(ev) {
             var _a, _b, _c, _d;
             ev.preventDefault();
-            const formData = new FormData(this);
-            const _player1Name = (_a = formData.get('player1')) === null || _a === void 0 ? void 0 : _a.toString();
-            const _player1Symbol = (_b = formData.get('player1Symbol')) === null || _b === void 0 ? void 0 : _b.toString().toUpperCase();
-            const _player2Name = (_c = formData.get('player2')) === null || _c === void 0 ? void 0 : _c.toString();
-            const _player2Symbol = (_d = formData.get('player2Symbol')) === null || _d === void 0 ? void 0 : _d.toString().toUpperCase();
-            const player1 = playerFactory(_player1Name === '' ? 'Player 1' : _player1Name !== null && _player1Name !== void 0 ? _player1Name : '', _player1Symbol === '' ? 'X' : _player1Symbol !== null && _player1Symbol !== void 0 ? _player1Symbol : '');
-            const player2 = playerFactory(_player2Name === '' ? 'Player 2' : _player2Name !== null && _player2Name !== void 0 ? _player2Name : '', _player2Symbol === '' ? 'O' : _player2Symbol !== null && _player2Symbol !== void 0 ? _player2Symbol : '');
-            log(player1);
-            log(player2);
-            //
-            //
-            //
-            //
-            //
-            const renderGameBoard = (function () {
-                const gameBoard = document.querySelector('.gameBoard');
-                log(gameBoard);
-                gameBoard === null || gameBoard === void 0 ? void 0 : gameBoard.style.setProperty('--grid-rows', '3');
-                gameBoard === null || gameBoard === void 0 ? void 0 : gameBoard.style.setProperty('--grid-cols', '3');
-                for (let i = 0; i <= 3; i++) {
-                    for (let j = 0; j <= 3; j++) {
-                        const gameCell = document.createElement('div');
-                        if (gameBoard) {
-                            gameBoard.appendChild(gameCell).className = 'gameCell';
-                            gameCell.setAttribute('data-key', `${i},${j}`);
+            if (onceFlag) {
+                const formData = new FormData(this);
+                const _player1Name = (_a = formData.get('player1')) === null || _a === void 0 ? void 0 : _a.toString();
+                const _player1Symbol = (_b = formData.get('player1Symbol')) === null || _b === void 0 ? void 0 : _b.toString();
+                const _player2Name = (_c = formData.get('player2')) === null || _c === void 0 ? void 0 : _c.toString();
+                const _player2Symbol = (_d = formData.get('player2Symbol')) === null || _d === void 0 ? void 0 : _d.toString();
+                const player1 = playerFactory(_player1Name === '' ? 'Player 1' : _player1Name !== null && _player1Name !== void 0 ? _player1Name : '', _player1Symbol === '' ? 'X' : _player1Symbol !== null && _player1Symbol !== void 0 ? _player1Symbol : '');
+                const player2 = playerFactory(_player2Name === '' ? 'Player 2' : _player2Name !== null && _player2Name !== void 0 ? _player2Name : '', _player2Symbol === '' ? 'O' : _player2Symbol !== null && _player2Symbol !== void 0 ? _player2Symbol : '');
+                //
+                //
+                //
+                //
+                //
+                const renderGameBoard = (function () {
+                    const gameBoard = document.querySelector('.gameBoard');
+                    gameBoard === null || gameBoard === void 0 ? void 0 : gameBoard.style.setProperty('--grid-rows', '3');
+                    gameBoard === null || gameBoard === void 0 ? void 0 : gameBoard.style.setProperty('--grid-cols', '3');
+                    for (let i = 0; i < 3; i++) {
+                        for (let j = 0; j < 3; j++) {
+                            const gameCell = document.createElement('div');
+                            if (gameBoard) {
+                                gameBoard.appendChild(gameCell).className = 'gameCell';
+                                gameCell.setAttribute('data-key', `${i},${j}`);
+                            }
+                            gameCell.addEventListener('click', handleCellClick);
                         }
-                        gameCell.addEventListener('click', handleCellClick);
                     }
-                }
-                function handleCellClick(ev) {
-                    log(this);
-                    log(ev);
-                }
-            })();
+                    //
+                    //
+                    //
+                    //
+                    //
+                    function handleCellClick() {
+                        var _a;
+                        const cellKey = (_a = this.dataset.key) !== null && _a !== void 0 ? _a : '';
+                        log(cellKey);
+                        renderMove(cellKey, player1, player2);
+                        checkWin(cellKey, cells);
+                    }
+                    //
+                    //
+                    //
+                    //
+                    //
+                    function renderMove(cellKey_, player1_, player2_) {
+                        const gameCellCurrent = document.querySelector(`.gameCell[data-key='${cellKey_}']`);
+                        const gameCellText = document.createElement('fieldset');
+                        gameCellCurrent === null || gameCellCurrent === void 0 ? void 0 : gameCellCurrent.appendChild(gameCellText);
+                        gameCellText.className = 'gameCellText';
+                        if (player1Turn && player2Turn)
+                            return null;
+                        if (!player1Turn && !player2Turn)
+                            return null;
+                        if (player1Turn && !player2Turn) {
+                            if (gameCellCurrent) {
+                                gameCellText.textContent = player1_.symbol;
+                                gameCellText.disabled = true;
+                            }
+                            player1Turn = false;
+                            player2Turn = true;
+                        }
+                        else if (!player1Turn && player2Turn) {
+                            if (gameCellCurrent) {
+                                gameCellText.textContent = player2_.symbol;
+                                gameCellText.disabled = true;
+                            }
+                            player1Turn = true;
+                            player2Turn = false;
+                        }
+                    }
+                    //
+                    //
+                    //
+                    //
+                    //
+                    function checkWin(cellKey_, cells_) {
+                        cells_.push(cellKey_.split(','));
+                        log(cells_);
+                    }
+                    //
+                    //
+                    //
+                })();
+                //
+                //
+                //
+            }
+            onceFlag = false;
         }
         //
         //
--- script.ts
+++ script.ts
@@ -4,6 +4,11 @@ const mainApp = function () {
 	type Form = HTMLFormElement | null
 	type Div = HTMLDivElement | null
 
+	type Player = {
+		name: string
+		symbol: string
+	}
+
 	//
 
 	const gameModule = function () {}
@@ -36,59 +41,127 @@ const mainApp = function () {
 	const gameBoardModule = (function () {
 		//grabs the elements and sets up event listeners
 		const form: Form = document.querySelector('.form-startBttn')
+		let onceFlag = true
+		let player1Turn = true
+		let player2Turn = false
+
+		let cells: Array<string[]> = []
 
 		function handleStartBttn(this: HTMLFormElement, ev: SubmitEvent) {
 			ev.preventDefault()
 
-			const formData = new FormData(this)
-			const _player1Name = formData.get('player1')?.toString()
-			const _player1Symbol = formData.get('player1Symbol')?.toString().toUpperCase()
-			const _player2Name = formData.get('player2')?.toString()
-			const _player2Symbol = formData.get('player2Symbol')?.toString().toUpperCase()
+			if (onceFlag) {
+				const formData = new FormData(this)
+				const _player1Name = formData.get('player1')?.toString()
+				const _player1Symbol = formData.get('player1Symbol')?.toString()
+				const _player2Name = formData.get('player2')?.toString()
+				const _player2Symbol = formData.get('player2Symbol')?.toString()
 
-			const player1 = playerFactory(
-				_player1Name === '' ? 'Player 1' : _player1Name ?? '',
-				_player1Symbol === '' ? 'X' : _player1Symbol ?? ''
-			)
-			const player2 = playerFactory(
-				_player2Name === '' ? 'Player 2' : _player2Name ?? '',
-				_player2Symbol === '' ? 'O' : _player2Symbol ?? ''
-			)
+				const player1 = playerFactory(
+					_player1Name === '' ? 'Player 1' : _player1Name ?? '',
+					_player1Symbol === '' ? 'X' : _player1Symbol ?? ''
+				)
+				const player2 = playerFactory(
+					_player2Name === '' ? 'Player 2' : _player2Name ?? '',
+					_player2Symbol === '' ? 'O' : _player2Symbol ?? ''
+				)
 
-			log(player1)
-			log(player2)
+				//
+				//
+				//
+				//
+				//
 
-			//
-			//
-			//
-			//
-			//
+				const renderGameBoard = (function () {
+					const gameBoard: Div = document.querySelector('.gameBoard')
 
-			const renderGameBoard = (function () {
-				const gameBoard: Div = document.querySelector('.gameBoard')
-				log(gameBoard)
+					gameBoard?.style.setProperty('--grid-rows', '3')
+					gameBoard?.style.setProperty('--grid-cols', '3')
 
-				gameBoard?.style.setProperty('--grid-rows', '3')
-				gameBoard?.style.setProperty('--grid-cols', '3')
+					for (let i = 0; i < 3; i++) {
+						for (let j = 0; j < 3; j++) {
+							const gameCell = document.createElement('div')
 
-				for (let i = 0; i <= 3; i++) {
-					for (let j = 0; j <= 3; j++) {
-						const gameCell = document.createElement('div')
+							if (gameBoard) {
+								gameBoard.appendChild(gameCell).className = 'gameCell'
+								gameCell.setAttribute('data-key', `${i},${j}`)
+							}
 
-						if (gameBoard) {
-							gameBoard.appendChild(gameCell).className = 'gameCell'
-							gameCell.setAttribute('data-key', `${i},${j}`)
+							gameCell.addEventListener('click', handleCellClick)
 						}
-
-						gameCell.addEventListener('click', handleCellClick)
 					}
-				}
 
-				function handleCellClick(this: HTMLDivElement, ev: MouseEvent) {
-					log(this)
-					log(ev)
-				}
-			})()
+					//
+					//
+					//
+					//
+					//
+
+					function handleCellClick(this: HTMLDivElement) {
+						const cellKey = this.dataset.key ?? ''
+						log(cellKey)
+
+						renderMove(cellKey, player1, player2)
+						checkWin(cellKey, cells)
+					}
+
+					//
+					//
+					//
+					//
+					//
+
+					function renderMove(cellKey_: string, player1_: Player, player2_: Player) {
+						const gameCellCurrent: Div = document.querySelector(
+							`.gameCell[data-key='${cellKey_}']`
+						)
+
+						const gameCellText = document.createElement('fieldset')
+						gameCellCurrent?.appendChild(gameCellText)
+						gameCellText.className = 'gameCellText'
+
+						if (player1Turn && player2Turn) return null
+						if (!player1Turn && !player2Turn) return null
+
+						if (player1Turn && !player2Turn) {
+							if (gameCellCurrent) {
+								gameCellText.textContent = player1_.symbol
+								gameCellText.disabled = true
+							}
+
+							player1Turn = false
+							player2Turn = true
+						} else if (!player1Turn && player2Turn) {
+							if (gameCellCurrent) {
+								gameCellText.textContent = player2_.symbol
+								gameCellText.disabled = true
+							}
+
+							player1Turn = true
+							player2Turn = false
+						}
+					}
+
+					//
+					//
+					//
+					//
+					//
+
+					function checkWin(cellKey_: string, cells_: Array<string[]>) {
+						cells_.push(cellKey_.split(','))
+						log(cells_)
+					}
+
+					//
+					//
+					//
+				})()
+				//
+				//
+				//
+			}
+			onceFlag = false
 		}
 		//
 		//
